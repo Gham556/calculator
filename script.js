9fbdcd65faea651722cd6be4838d68f3bc2db47a
George <georgea.fotheringham@gmail.com>
2022-07-29 19:22:31 -0400

    defer script to fix node list

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ const divide = function(a, b) {
 const mult = function(a, b) {
     return a * b
 };
+
+
+const numberButtons = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.op');
+
+// numberButtons.forEach().addEventListener('click', store);
+
+
+console.log(numberButtons)
+console.log(operatorButtons)
+
+numberButtons.forEach(setEvent);
+
+function setEvent() {
+    addEventListener('click', store)
+}
+
+function store() {
+    
+}
+
+const operate = function(numOne, operator, num2) {
+        result = numOne, operator, num2 
+}
+
