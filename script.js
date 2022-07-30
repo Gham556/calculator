1b99e4d39f434095eec0aa5c7f6685241f931f02
George <georgea.fotheringham@gmail.com>
2022-07-29 20:22:38 -0400

    display works, equals button does nothing

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const mult = function(a, b) {
 const numberButtons = document.querySelectorAll('.num');
 const operatorButtons = document.querySelectorAll('.op');
 const display = document.querySelector('.display');
+const equals = document.querySelector('.equals');
 
 
 numberButtons.forEach((number) =>{
@@ -27,17 +28,39 @@ numberButtons.forEach((number) =>{
 });
 
 operatorButtons.forEach((number) =>{
-    number.addEventListener('click', operate)
-})
+    number.addEventListener('click', operate);
+});
+
+equals.addEventListener('click', end);
 
 function store() { 
+    
+    numberButtons.forEach((number) =>{
+        number.removeEventListener('click', reset)});
     display.textContent += this.textContent;
-}
+};
 
 function operate() {
     initialNum = display.textContent;
     display.textContent = this.textContent;
+    numberButtons.forEach((number) =>{
+        number.removeEventListener('click', store)
+        number.addEventListener('click', reset);
+    });
+    return initialNum;
+};
 
+function reset () {
+    operator = display.textContent; 
+    display.textContent = this.textContent
+    numberButtons.forEach((number) =>{
+        number.addEventListener('click', store);
+        number.removeEventListener('click', reset)});
+   ;
+    return operator
 }
 
-
+function end () {
+    thig = operate() + reset() + display.textContent;
+    display.textContent = thig
+};
